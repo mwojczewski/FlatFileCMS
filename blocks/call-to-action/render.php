d5f3c03aad8642127d5524682324b0f427865971
mwojczewski <mwojczewski@outlook.com>
2026-09-05 12:12:19 +0200

    docs added

--- blocks/call-to-action/render.php
+++ blocks/call-to-action/render.php
@@ -9,6 +9,12 @@ declare(strict_types=1);
         <p><?= $context->escape($data['description']) ?></p>
         <a class="button button--light"
             href="<?= $context->url($data['button_url']) ?>"><?= $context->escape($data['button_label']) ?> <span
-                aria-hidden="true">↗</span></a>
+                aria-hidden="true">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    class="bi bi-arrow-up-right" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd"
+                        d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z" />
+                </svg>
+            </span></a>
     </div>
 </section>
